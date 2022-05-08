46e560fa2bd6e3b3f2d34b6aea1cfd2b4229b687
Gowtham Punniyaseelan <88957910+gowthampunniyaseelan@users.noreply.github.com>
2022-05-08 08:12:35 +0530

    authentication

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Page } from './views/Page';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // new imports
 import Signup from './authentication/Signup';
 import Login from './authentication/Login';
 import Home from './Home';
 function App() {
+  const [isAuthenticated,setIsAuthenticated] = useState(()=>{localStorage.getItem('token')});
   return(
     <Router>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/signup" element={<Signup/>} />
       <Route path="/login" element={<Login/>}/>
-      <Route path="/page" element={<Page />} />
+      <Route path="/page" element={
+      isAuthenticated ? <Page/> : <Login/>
+      } />
     </Routes>
     </Router>
   );
